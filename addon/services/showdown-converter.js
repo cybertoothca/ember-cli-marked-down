1813d5c60561a5775ffab8a92d565f1193bbd2a0
Dan Nelson <dnelson@cybertooth.ca>
2016-11-05 22:36:22 -0600

    Add a demonstration of the set-links-target component and also fix a log message so it points to the correct url

--- addon/services/showdown-converter.js
+++ addon/services/showdown-converter.js
@@ -16,7 +16,7 @@ export default Ember.Service.extend({
     // set showdown global settings from the environment
     if (Ember.isEmpty(config.APP.showdown)) {
       Ember.Logger.info('The `ember-cli-marked-down` addon will use ShowdownJS defaults to create the Converters.');
-      Ember.Logger.info('If you want to customize ShowdownJS behaviour, please see the ember-cli-marked-down README: https://github.com/cybertoothca/ember-cli-marked-down#showdownjs-configuration');
+      Ember.Logger.info('If you want to customize ShowdownJS behaviour, please see the ember-cli-marked-down README: https://github.com/cybertoothca/ember-cli-marked-down#showdownjs-configuration-optional');
       return;
     }
     const showdownConfig = Ember.Object.create(config.APP.showdown);
