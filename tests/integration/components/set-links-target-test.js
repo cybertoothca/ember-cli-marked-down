24a994c3a2bbe56a98c380325dc90e3bb8f907ca
nadnoslen <dan@cybertooth.io>
2022-04-06 00:22:01 -0600

    :recycle: Adding Ember-Cli-Update

--- tests/integration/components/set-links-target-test.js
+++ tests/integration/components/set-links-target-test.js
@@ -1,50 +1,52 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('set-links-target', 'Integration | Component | set links target', {
-  integration: true,
-  includes: [
-    'helper:marked-down'
-  ]
-});
+moduleForComponent(
+  "set-links-target",
+  "Integration | Component | set links target",
+  {
+    integration: true,
+    includes: ["helper:marked-down"],
+  }
+);
 
-test('when the yield does not include any links', function (assert) {
+test("when the yield does not include any links", function (assert) {
   this.render(hbs`
     {{#set-links-target}}
       {{marked-down "### Heading 3"}}
     {{/set-links-target}}
   `);
-  assert.equal(this.$('div').html().trim(), '<h3 id="heading3">Heading 3</h3>');
+  assert.equal(this.$("div").html().trim(), '<h3 id="heading3">Heading 3</h3>');
 });
 
-test('when the yields link(s) do not have a target value, _blank is added', function (assert) {
+test("when the yields link(s) do not have a target value, _blank is added", function (assert) {
   this.render(hbs`
     {{#set-links-target}}
       {{marked-down "[Some Link](http://github.com)"}}
     {{/set-links-target}}
   `);
-  assert.equal(this.$('a').attr('target'), '_blank');
+  assert.equal(this.$("a").attr("target"), "_blank");
 });
 
-test('when excludeSelfLinks is false, the target is applied to local links', function (assert) {
+test("when excludeSelfLinks is false, the target is applied to local links", function (assert) {
   this.render(hbs`
     {{#set-links-target excludeSelfLinks?=false}}
       {{marked-down "[Some Link](http://localhost:7357/some/path)"}}
     {{/set-links-target}}
   `);
-  assert.equal(this.$('a').attr('target'), '_blank');
+  assert.equal(this.$("a").attr("target"), "_blank");
 });
 
-test('when excludeSelfLinks is true, the target is not applied to such links', function (assert) {
+test("when excludeSelfLinks is true, the target is not applied to such links", function (assert) {
   this.render(hbs`
-    {{#set-links-target}}
+    {{#set-links-target excludeSelfLinks?=true}}
       {{marked-down "[Some Link](http://localhost:7357/some/path)"}}
     {{/set-links-target}}
   `);
-  assert.equal(this.$('a').attr('target'), undefined);
+  assert.equal(this.$("a").attr("target"), undefined);
 });
 
-test('when the target is set for one of several links', function (assert) {
+test("when the target is set for one of several links", function (assert) {
   this.render(hbs`
     {{#set-links-target}}
       {{marked-down "
@@ -54,6 +56,6 @@ test('when the target is set for one of several links', function (assert) {
 "}}
     {{/set-links-target}}
   `);
-  assert.equal(this.$('a:eq(0)').attr('target'), undefined);
-  assert.equal(this.$('a:eq(1)').attr('target'), '_blank');
+  assert.equal(this.$("a:eq(0)").attr("target"), undefined);
+  assert.equal(this.$("a:eq(1)").attr("target"), "_blank");
 });
